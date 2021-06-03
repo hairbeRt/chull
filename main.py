b55e15ab9bb15bf2e9c3fdd56670223572189584
Jonas Cremer <jonascremer@uni-bonn.de>
2021-06-03 15:53:35 +0200

    Cleanup, refresh code for later experiments

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
-from phull import Point, convex_hull
+from phull import Point
 from matplotlib import pyplot as plt
 import numpy as np
 
-data = [Point(np.random.rand(),np.random.rand()) for i in range(0,50)]
-ch = convex_hull(data)
-cxs = [A._x for A in ch]
-cys = [A._y for A in ch]
-xs = [A._x for A in data]
-ys = [A._y for A in data]
-plt.scatter(xs, ys)
-plt.plot(cxs, cys)
+def get_random_hull_length(n: int) -> float:
+    data = [Point(*np.random.multivariate_normal([0,0],[[1,0],[0,1]])) for i in range(0,n)]
+    return Point.convex_hull_length(data)
 
-plt.show()
+
+def experiment(lower_n: int, upper_n: int, step_sz: int, am_exps: int):
+    ns = range(lower_n,upper_n, step_sz)
+    avg_lengths = []
+    for n in ns:
+        sum = 0
+        for i in range(0,am_exps):
+            sum += get_random_hull_length(n)
+        #Value to actually calculate. In this case, average length of convex hull boundary
+        val = sum/am_exps
+
+        avg_lengths.append(val)
+        print(n, val)
+    plt.plot(ns, avg_lengths)
+    plt.show()
+
+if __name__ = "__main__":
+    experiment(5,1000, 5, 10)
--- phull.py
+++ phull.py
@@ -23,29 +23,44 @@ class Point:
 
     def above(self, X: Point) -> bool:
         return not self.below(X)
+
+    @staticmethod
+    def distance(P: Point, Q: Point):
+        return ((P._x - Q._x)**2 + (P._y - Q._y)**2)**0.5
+
     @staticmethod
     def is_upper_triangle(A: Point, B: Point, C:Point) -> bool:
         assert A.leftof(B) and B.leftof(C)
         abslope = (B._y - A._y)/(B._x - A._x)
         acslope = (C._y - A._y)/(C._x - A._x)
         return abslope > acslope
+
     @staticmethod
     def is_lower_triangle(A: Point, B: Point, C: Point) -> bool:
         assert A.leftof(B) and B.leftof(C)
         return not Point.is_upper_triangle(A,B,C)
 
-def convex_hull(S):
-    S.sort(key=lambda Q: Q._x)
-    upperhull = []
-    lowerhull = []
-    for P in S:
-        upperhull.append(P)
-        lowerhull.append(P)
+    @staticmethod
+    def convex_hull(S):
+        S.sort(key=lambda Q: Q._x)
+        upperhull = []
+        lowerhull = []
+        for P in S:
+            upperhull.append(P)
+            lowerhull.append(P)
 
-        while len(upperhull) >= 3 and not Point.is_upper_triangle(upperhull[-3], upperhull[-2], upperhull[-1]):
-            del upperhull[-2]
-        while len(lowerhull) >= 3 and not Point.is_lower_triangle(lowerhull[-3], lowerhull[-2], lowerhull[-1]):
-            del lowerhull[-2]
-    upperhull.reverse()
-    del upperhull[0]
-    return lowerhull + upperhull
+            while len(upperhull) >= 3 and not Point.is_upper_triangle(upperhull[-3], upperhull[-2], upperhull[-1]):
+                del upperhull[-2]
+            while len(lowerhull) >= 3 and not Point.is_lower_triangle(lowerhull[-3], lowerhull[-2], lowerhull[-1]):
+                del lowerhull[-2]
+        upperhull.reverse()
+        del upperhull[0]
+        return lowerhull + upperhull
+
+    @staticmethod
+    def convex_hull_length(S):
+        hull = Point.convex_hull(S)
+        result = 0.
+        for i in range(1,len(hull)):
+            result += Point.distance(hull[i], hull[i-1])
+        return result
